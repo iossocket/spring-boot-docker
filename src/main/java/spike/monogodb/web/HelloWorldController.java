16e152b79323c926dadef0d61348f0e1864a1d2b
Liang Hong <lianghongbuaa@gmail.com>
2016-12-19 11:09:37 +0800

    Environment OK

---
+++ src/main/java/spike/monogodb/web/HelloWorldController.java
@@ -0,0 +1,15 @@
+package spike.monogodb.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping(method = RequestMethod.GET,value = "/helloworld")
+    public ResponseEntity helloWorld () {
+        return new ResponseEntity("HelloWorld", HttpStatus.OK);
+    }
+}
---
+++ src/test/java/spike/monogodb/web/HelloWorldControllerTest.java
@@ -0,0 +1,18 @@
+package spike.monogodb.web;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class HelloWorldControllerTest {
+    @Test
+    public void shouldReturnHelloWorldWithOK () {
+        //given
+        //when
+        ResponseEntity responseEntity = new HelloWorldController().helloWorld();
+        //then
+        Assert.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        Assert.assertEquals("HelloWorld", responseEntity.getBody().toString());
+    }
+}
